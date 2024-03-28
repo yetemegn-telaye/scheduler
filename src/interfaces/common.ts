ec8883fa9fefc9c89c81f5e0ba26bbfd353aaca7
yetemegn-telaye <lemma19yet@gmail.com>
2024-03-28 20:27:35 +0300

    Create interfaces

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -5,3 +5,20 @@ export interface ICalenderInput{
     events:any
     
 }
+
+export interface Label {
+    id: number;
+    text: string;
+    color: string;
+
+  }
+
+export interface Task {
+  id: number;
+  title: string;
+  labels: Label[]; // Array of labels
+}
+
+export interface TaskMap {
+  [date: string]: Task[];
+}
